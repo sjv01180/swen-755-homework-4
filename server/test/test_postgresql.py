d4c7e1931fd3f16053e53d759466ec81cf65192a
Samuel Velasquez <samuelvelasquez@protonmail.com>
2024-11-18 20:49:35 -0500

    testing postgres connection on container

--- server/test/test_postgresql.py
+++ server/test/test_postgresql.py
@@ -7,6 +7,7 @@ class TestPostgreSQL(unittest.TestCase):
         conn = connect()
         cur = conn.cursor()
         cur.execute('SELECT VERSION()')
+        print(cur.fetchone()[0])
         self.assertTrue(cur.fetchone()[0].startswith('PostgreSQL'))
         conn.close()
 
